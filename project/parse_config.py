7bf3369fbe8f74d54b8ea345ec5cecd137826bd7
chenyx512 <yuxuanchen@stmarksschool.org>
2019-05-12 15:11:18 -0400

    Add config parser

---
+++ project/parse_config.py
@@ -0,0 +1,84 @@
+from datetime import datetime
+from functools import reduce
+from operator import getitem
+from pathlib import Path
+
+from utils.utils import read_yaml, write_yaml
+
+
+class ConfigParser:
+
+    def __init__(self, args, options=None):
+        for opt in options:
+            args.add_argument(*opt.flags, default=None, type=opt.type)
+        args = args.parse_args()
+
+        if args.resume:
+            self.resume = Path(args.resume)
+            self.config_path = self.resume.parent / 'config.yaml'
+        else:
+            assert args.config is not None, 'config not specified'
+            self.resume = None
+            self.config_path = Path(args.config)
+
+        config = read_yaml(self.config_path)
+        self.__config = _update_config(config, options, args)
+
+        save_dir = Path(self.config['save_dir'])
+        exper_name = self.config['name']
+        timestamp = datetime.now().strftime(r'%m%d_%H%M%S')
+        self.__save_dir = save_dir / 'models' / exper_name / timestamp
+        self.__log_dir = save_dir / 'log' / exper_name / timestamp
+
+        self.save_dir.mkdir(parents=True, exist_ok=True)
+        self.log_dir.mkdir(parents=True, exist_ok=True)
+
+        write_yaml(self.config, self.save_dir / 'config.yaml')
+
+    def initialize(self, name, module, *args):
+        """
+        finds a function handle with the name given as 'type' in config, and returns the
+        instance initialized with corresponding keyword args given as 'args'.
+        """
+        module_config = self[name]
+        return getattr(module, module_config['type'])(*args, **module_config['args'])
+
+    def __getitem__(self, name):
+        return self.config[name]
+
+    @property
+    def config(self):
+        return self.__config
+
+    @property
+    def log_dir(self):
+        return self.__log_dir
+
+    @property
+    def save_dir(self):
+        return self.__save_dir
+
+
+def _update_config(config, options, args):
+    for option in options:
+        value = getattr(args, _get_opt_name(option))
+        if value is not None:
+            _set_by_path(config, option.target, value)
+    return config
+
+
+def _get_opt_name(option):
+    for flag in option.flags:
+        if flag.startswith('--'):
+            return flag[2:]
+    raise AttributeError('the option has no verbose flag')
+
+
+def _set_by_path(tree, keys, value):
+    """Set a value in a nested object in tree by sequence of keys."""
+    _get_by_path(tree, keys[:-1])[keys[-1]] = value
+
+
+def _get_by_path(tree, keys):
+    """Access a nested object in tree by sequence of keys."""
+    return reduce(getitem, keys, tree)
---
+++ project/train.py
@@ -0,0 +1,28 @@
+import argparse
+import collections
+
+import data_loader.data_loaders as data_loader_module
+import data_loader.dataset as dataset_module
+from parse_config import ConfigParser
+
+
+def main(config):
+    dataset = config.initialize('dataset', dataset_module)
+    train_loader = config.initialize('data_loader', data_loader_module, dataset)
+    test_loader = train_loader.get_test_loader()
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument('-c', '--config', default=None, type=str,
+                      help='config file path (default: None)')
+    args.add_argument('-r', '--resume', default=None, type=str,
+                      help='path to latest checkpoint (default: None)')
+
+    CustomArgs = collections.namedtuple('CustomArgs', 'flags type target')
+    options = [
+        CustomArgs(['-lr', '--learning_rate'], type=float, target=('optimizer', 'args', 'lr')),
+        CustomArgs(['-bs', '--batch_size'], type=int, target=('data_loader', 'args', 'batch_size'))
+    ]
+    config = ConfigParser(args, options)
+    main(config)
